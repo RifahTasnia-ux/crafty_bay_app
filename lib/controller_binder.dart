b236cc5401445b4b5e351d18c3bb63038a07a5e9
rifahtasnia <rifahtasnia5962@gmail.com>
2024-07-01 00:15:40 +0600

    Update Commit

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -13,6 +13,7 @@ import 'package:crafty_bay/presentation/state_holders/product_list_by_category_c
 import 'package:crafty_bay/presentation/state_holders/read_profile_controller.dart';
 import 'package:crafty_bay/presentation/state_holders/review_list_controller.dart';
 import 'package:crafty_bay/presentation/state_holders/special_product_list_controller.dart';
+import 'package:crafty_bay/presentation/state_holders/user_auth_controller.dart';
 import 'package:crafty_bay/presentation/state_holders/verify_email_controller.dart';
 import 'package:crafty_bay/presentation/state_holders/verify_otp_controller.dart';
 import 'package:crafty_bay/presentation/state_holders/wish_list_controller.dart';
@@ -39,5 +40,6 @@ class ControllerBinder extends Bindings {
     Get.put(CartListController());
     Get.put(ReviewListController());
     Get.put(AddReviewController());
+    Get.put(UserAuthController());
   }
 }
